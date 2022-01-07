ede5332f0e6ff677822e5a30759a861ea608812e
JuniorDeveloper1 <nickje.debut@gmail.com>
2022-01-08 00:41:36 +0100

    Config

--- src/main/java/com/juniordeveloper/MiningSystem/MineMain.java
+++ src/main/java/com/juniordeveloper/MiningSystem/MineMain.java
@@ -1,6 +1,7 @@
 package com.juniordeveloper.MiningSystem;
 
 
+import com.juniordeveloper.MiningSystem.config.ConfigAchievement;
 import com.juniordeveloper.MiningSystem.events.onBreak;
 import com.juniordeveloper.MiningSystem.events.onJoin;
 import com.juniordeveloper.MiningSystem.messages.MessageManager;
@@ -14,6 +15,7 @@ import org.jetbrains.annotations.Nullable;
 
 public class MineMain extends JavaPlugin {
     public static HikariDataSource hikariDataSource;
+
     public static MineMain instance;
 
     public static MineMain getInstance() {return instance;}
@@ -36,12 +38,12 @@ public class MineMain extends JavaPlugin {
     @Override
     public void onEnable() {
         instance = this;
-        Listeners();
         hikariDataSource = new HikariDataSource(new HikariConfig(){{
             super.setJdbcUrl("sqlite://G:\\Coding\\SQLLITE\\DATABASE.db");
         }});
 
-
+        Listeners();
+        Configs();
         System.out.println(MessageManager.PLUGIN_ENABLE.getMessage());
 
     }
@@ -50,5 +52,8 @@ public class MineMain extends JavaPlugin {
         Bukkit.getServer().getPluginManager().registerEvents(new onBreak(), this);
         Bukkit.getServer().getPluginManager().registerEvents(new onJoin(), this);
 
+    }
+    public void Configs() {
+        ConfigAchievement.createAchievementFile();
     }
 }
---
+++ src/main/java/com/juniordeveloper/MiningSystem/config/ConfigAchievement.java
@@ -0,0 +1,41 @@
+package com.juniordeveloper.MiningSystem.config;
+
+import com.juniordeveloper.MiningSystem.MineMain;
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import java.io.File;
+import java.io.IOException;
+
+public class ConfigAchievement  {
+
+    private static MineMain plugin = MineMain.getInstance();
+    private static String achievementConfigName = "achievement.yml";
+
+
+
+
+    private static File achievementConfigFile;
+    private static FileConfiguration achievementConfig;
+
+    public static  FileConfiguration getAchievementConfig() {
+        return achievementConfig;
+    }
+
+    public  static void createAchievementFile() {
+        achievementConfigFile = new File(plugin.getDataFolder(), achievementConfigName);
+        if(!achievementConfigFile.exists()) {
+            achievementConfigFile.getParentFile().mkdirs();
+            plugin.saveResource(achievementConfigName, false);
+        }
+
+        achievementConfig = new YamlConfiguration();
+        try {
+            achievementConfig.load(achievementConfigFile);
+
+        }catch (IOException | InvalidConfigurationException e) {
+            e.printStackTrace();
+        }
+    }
+}
